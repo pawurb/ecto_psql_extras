3b18aa8a7ad041aa3192c1220224940c9e665b5c
Dylan Markow <dylan@dylanmarkow.com>
2020-10-21 08:22:02 -0500

    Handle different columns for older versions of pg_stat_statements

--- lib/queries/calls_legacy.ex
+++ lib/queries/calls_legacy.ex
@@ -1,11 +1,11 @@
-defmodule EctoPSQLExtras.Outliers do
+defmodule EctoPSQLExtras.CallsLegacy do
   @behaviour EctoPSQLExtras
 
   def info do
     %{
-      title: "10 queries that have longest execution time in aggregate",
-      order_by: [exec_time: :desc],
+      title: "10 queries that have the highest frequency of execution",
       limit: 10,
+      order_by: [calls: :desc],
       columns: [
         %{name: :query, type: :string},
         %{name: :exec_time, type: :interval},
@@ -18,15 +18,15 @@ defmodule EctoPSQLExtras.Outliers do
 
   def query do
     """
-    /* 10 queries that have longest execution time in aggregate */
+    /* 10 queries that have the highest frequency of execution */
 
     SELECT query AS query,
-    interval '1 millisecond' * total_exec_time AS exec_time,
-    (total_exec_time/sum(total_exec_time) OVER()) AS prop_exec_time,
+    interval '1 millisecond' * total_time AS exec_time,
+    (total_time/sum(total_time) OVER())  AS exec_time_ratio,
     calls,
     interval '1 millisecond' * (blk_read_time + blk_write_time) AS sync_io_time
     FROM pg_stat_statements WHERE userid = (SELECT usesysid FROM pg_user WHERE usename = current_user LIMIT 1)
-    ORDER BY total_exec_time DESC
+    ORDER BY calls DESC
     LIMIT 10;
     """
   end
