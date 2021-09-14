7e3920f4843c53192b6f8eab5d77775afdc0dbae
Philip Sampaio <philip.sampaio@gmail.com>
2021-09-14 17:17:22 -0300

    Use `Node.self()` instead of starting a new one
    Trying to prevent errors on CI.

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,9 +1,6 @@
 Code.require_file "support/test_repo.exs", __DIR__
 
-node_name = :"primary@127.0.0.1"
-
-:net_kernel.start([node_name])
-Application.put_env(:ecto_psql_extras, :node_name, node_name)
+Application.put_env(:ecto_psql_extras, :node_name, Node.self())
 
 Application.put_env(:dummy_app, Dummy.Repo, [url: EctoPSQLExtras.TestRepo.database_url()])
 
