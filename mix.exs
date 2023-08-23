ab3341739eed3eb70f83e24bb3a7b02943977b41
pawurb <contact@pawelurbanek.com>
2023-08-23 15:50:22 +0200

    Version bump

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule EctoPSQLExtras.Mixfile do
   use Mix.Project
   @github_url "https://github.com/pawurb/ecto_psql_extras"
-  @version "0.7.12"
+  @version "0.7.13"
 
   def project do
     [
