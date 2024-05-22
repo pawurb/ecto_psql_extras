d82c049e693c6172abc15e979007d4b47d6bcc42
Wilhelm Kirschbaum <wkirschbaum@users.noreply.github.com>
2024-05-22 11:41:11 +0200

    Update postgrex version to allow 0.18 (#53)
    The only deprecation is the removal of the ssl_opts option, which
    don't seem to be used in this project.

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule EctoPSQLExtras.Mixfile do
   use Mix.Project
   @github_url "https://github.com/pawurb/ecto_psql_extras"
-  @version "0.7.15"
+  @version "0.7.16"
 
   def project do
     [
@@ -20,7 +20,7 @@ defmodule EctoPSQLExtras.Mixfile do
     [
       {:table_rex, "~> 3.1.1 or ~> 4.0.0"},
       {:ecto_sql, "~> 3.7"},
-      {:postgrex, "~> 0.16.0 or ~> 0.17.0"},
+      {:postgrex, "~> 0.16.0 or ~> 0.17.0 or ~> 0.18.0"},
       {:ex_doc, ">= 0.30.0", only: :dev, runtime: false},
       {:mock, "~> 0.3.0", only: :test}
     ]
